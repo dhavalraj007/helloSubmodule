04bb4492fe8d938ea97704d239491980d0292085
Dhaval Raj <rdjcoding@gmail.com>
2023-07-12 20:34:07 +0530

    init

---
+++ hello.go
@@ -0,0 +1,9 @@
+package greetings
+
+import "fmt"
+import "rsc.io/quote"
+
+func Hello(name string) string {
+	message:= fmt.Sprintf("Hi %v, a quote for you: %v",name,quote.Go())
+	return message
+}
